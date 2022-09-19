026a8aacd626b9cfff2e2bec43913cf0683e35a1
ellrub <ellefsen88@gmail.com>
2022-09-20 00:02:08 +0200

    Additions to contact |
    Added working contact form
    Removed captcha
    Added a honeypot

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -6,22 +6,36 @@ export default function Contact() {
         <section className="contact-me">
             <h2 className="contact-h2">Contact Me</h2>
             <div className="contact-card group">
-                <div className="input-group">
-                    <label for="text">Your Full Name</label>
-                    <input required="" type="text" id="text" placeholder="Your Full Name"/>
-                </div>
-                <div className="input-group">
-                    <label for="email">Your Email</label>
-                    <input required="" type="email" id="email" placeholder="Your Email"/>
-                </div>
-                <div className="input-group">
-                    <label for="subject">Subject</label>
-                    <input required="" type="subject" id="subject" placeholder="Subject"/>
-                </div>
-                <div className="input-group">
-                    <label for="text">Message</label>
-                    <textarea name="" id="" cols="30" rows="5" placeholder="Hey, what's on your mind?"></textarea>
-                </div>
+                <form action="https://formsubmit.co/ef4dd30446c32a85bf7ebbdf31bc7e2d" method="POST">
+                    {/* -- Honeypot -- */}
+                    <input type="text" name="_honey" className="honeypot"/>
+
+                    {/* -- Disable Captcha -- */}
+                    <input type="hidden" name="_captcha" value="false"/>
+
+                    <input type="hidden" name="_next" value="https://portfolio-seven-puce-51.vercel.app/" />
+
+                    <div className="input-group">
+                        <label for="text">Your Full Name</label>
+                        <input className="name" name="Name" required="" type="text" id="text" 
+                                placeholder="Your Full Name" autoComplete="off"/>
+                    </div>
+                    <div className="input-group">
+                        <label for="email">Your Email</label>
+                        <input required="" type="email" id="email" name="Email" 
+                                placeholder="Your Email" autoComplete="off"/>
+                    </div>
+                    <div className="input-group">
+                        <label for="subject">Subject</label>
+                        <input className="subject" required="" name="Subject" type="text" id="subject" 
+                                placeholder="Subject" autoComplete="off"/>
+                    </div>
+                    <div className="input-group">
+                        <label for="text">Message</label>
+                        <textarea name="Comment" id="" cols="30" rows="5" placeholder="Hey, what's on your mind?" autoComplete="off"></textarea>
+                    </div>
+                    <button className="contact-btn">Send</button>
+                </form>
             </div>
         </section>
     )
--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav className="navbar">
             <h3>Placeholder</h3>
             <label for="check" class="bar">
-                <input id="check" type="checkbox" />
+                <input className="nav-menu" id="check" type="checkbox" />
                 <span class="top"></span>
                 <span class="middle"></span>
                 <span class="bottom"></span>
