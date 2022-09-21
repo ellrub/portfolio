d1cfb69a92222f060ae14f72fd1c2cde46fd66e8
ellrub <ellefsen88@gmail.com>
2022-09-21 22:31:11 +0200

    Navbar
    Added navmenu on navbar

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -5,6 +5,23 @@ export default function Navbar() {
     return (
         <nav className="navbar">
             <h3>Placeholder</h3>
+            <i className="fa-solid fa-bars" id="menu"></i>
+            <div id="overlay">
+            <i class="fa-solid fa-x"></i>
+                <p>Home</p>
+                <p>About Me</p>
+                <p>My Projects</p>
+                <p>My Blog</p>
+                <p>Contact Me</p>
+            </div>
         </nav>
     )
 }
+
+// document.getElementById("menu").addEventListener("click", function() {
+//     document.getElementById("overlay").style.display = "block"
+// })
+
+// document.getElementById("menu").addEventListener("click", function() {
+//     document.getElementById("menu").style.display = "none"
+// })
--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -4,3 +4,11 @@ import App from './App'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+document.getElementById("menu").addEventListener("click", function() {
+    document.getElementById("overlay").style.display = "block"
+})
+
+document.getElementById("menu").addEventListener("click", function() {
+    document.getElementById("menu").style.display = "none"
+})
